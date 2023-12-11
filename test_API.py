de9e9accdb0c6b2f75b49346f1a6a312e214e394
crypto_tyan <cryptotyantrader@gmail.com>
2023-12-11 16:09:50 +0100

    init

---
+++ test_API.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from main_config import get_price, money
+
+
+app = FastAPI()
+
+
+@app.get("/price")
+def show_price() -> float:
+    return get_price()
+
+
+@app.get("/money")
+def show_money() -> float:
+    return money()
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+
+
